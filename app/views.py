4e26cdbd04087f19b51ae74bd6cd9418c4f289c6
parvathikanchimireddy <parvathikanchimireddy@gmail.com>
2023-04-05 17:30:11 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from app.models import *
+
+# Create your views here.
+def display_topics(request):
+    LOT=Topic.objects.all()
+    d={'topics':LOT}
+    return render(request,'display_topics.html',context=d)
+
+def display_webpages(request):
+    LOW=Webpage.objects.all()
+    d={'webpages':LOW}
+    return render(request,'display_webpages.html',context=d)
+
+def display_accessrecords(request):
+    LOA=Accessrecord.objects.all()
+    d={'accessrecords':LOA}
+    return render(request,'display_accessrecords.html',context=d)
+
